70eb86b409d7e9dddd6855b2d42d7a3ba9cd74ef
BRB3D <samuraix221@hotmail.com>
2022-04-10 15:49:10 -0400

    axios install

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css"
 import Reacr from "react";
 import Field from "./components/field";
-import Languages from "./components?languages";
+import Languages from "./components/languages";
 import Translate from "./components/translate"
 
 export default function App() {
